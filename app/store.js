552ad7f01368f6457c4547b52a2a3bcea98659ff
wayneleon1 <rurangwaleon10@gmail.com>
2023-11-08 17:23:16 +0200

    Adding Logger Middleware in reducer for tracking action that happened

--- app/store.js
+++ app/store.js
@@ -1,11 +1,16 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
+const { getDefaultMiddleware } = require("@reduxjs/toolkit");
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
+const reduxLogger = require("redux-logger");
+
+const logger = reduxLogger.createLogger();
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecrem: icecreamReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 module.exports = store;
