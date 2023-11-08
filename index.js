340b91c55c28af1d0a999e05ea5e93f5ef4ef02f
wayneleon1 <rurangwaleon10@gmail.com>
2023-11-08 16:21:16 +0200

    Create iceCream feature

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+const icecreamActions =
+  require("./features/icecream/icecreamSlice").icecreamActions;
 
 console.log("Initial State", store.getState());
 const unsubscribe = store.subscribe(() => {
@@ -10,4 +12,9 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
+
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(2));
+
 unsubscribe();
