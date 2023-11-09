ecaac224ee4423ad30ec576b511a8be3221013be
wayneleon1 <rurangwaleon10@gmail.com>
2023-11-09 21:36:27 +0200

    Introduce Provider componnt

--- react-pro/src/App.jsx
+++ react-pro/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+
 import CakeView from "./features/cake/CakeView";
 import IcecreamView from "./features/icecream/IcecreamView";
 import UserView from "./features/user/UserView";
